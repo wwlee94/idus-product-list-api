ac18375ef62861954492f7217a8c1f93fee43ebc
wwlee94 <wwlee94@naver.com>
2019-11-29 12:51:55 +0900

    Exception 코드 수정 및 callback 데이터 id -> 정수형으로 변경

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show]
 
-  rescue_from Exceptions::BadRequest, Exceptions::InternalServerError do |e|
+  rescue_from Exceptions::BadRequest, Exceptions::NotFound, Exceptions::InternalServerError do |e|
     response = { statusCode: e.status, body: e.message }
     render json: JSON.pretty_generate(response)
   end
@@ -20,16 +20,13 @@ class ProductsController < ApplicationController
       limit: 50
     }
   
+    # 페이지 변수가 있을 때
     if params[:page]
-      puts 'page 변수 존재!' 
       raise Exceptions::ParameterIsNotInteger unless is_number? params[:page]
 
       page_no = params[:page].to_i
       raise Exceptions::PageUnderRequest if page_no <= 0
 
-      # @products.items.each{ |product|
-      #   product.transform_values(&:to_i)
-      # }
       start_id = (params[:page].to_i - 1) * 50
 
       puts('Searching for more ...')
@@ -39,19 +36,31 @@ class ProductsController < ApplicationController
         stat: "ok"
       }
 
-      @products = dynamodb.query(param)
-
-      raise Exceptions::PageOverRequest if @products.items.blank?
-
-      puts @products.items
-      puts @products.last_evaluated_key
-
-      response = { statusCode: 200, body: @products.items }
-      render json: JSON.pretty_generate(response)
-
-    else
       begin
         @products = dynamodb.query(param)
+        puts @products.items
+        puts @products.last_evaluated_key
+        raise Exceptions::PageOverRequest if @products.items.blank?
+        @products.items.each do |item|
+          str_int = item['id']
+          item['id'] = str_int.to_i
+        end
+        response = { statusCode: 200, body: @products.items }
+      rescue Aws::DynamoDB::Errors::ServiceError => e
+        response = { statusCode: 500, body: "#{e.message}" }
+      end
+
+      raise Exceptions::InternalServerError if response.nil?
+
+      render json: JSON.pretty_generate(response)
+    else
+      # 페이지 변수 없을 때 첫 50개 데이터만 보여줌
+      begin
+        @products = dynamodb.query(param)
+        @products.items.each do |item|
+          str_int = item['id']
+          item['id'] = str_int.to_i
+        end
         response = { statusCode: 200, body: @products.items }
       rescue Aws::DynamoDB::Errors::ServiceError => e
         response = { statusCode: 500, body: "#{e.message}" }
@@ -61,14 +70,13 @@ class ProductsController < ApplicationController
       
       render json: JSON.pretty_generate(response)
     end
-
-  rescue Exceptions::PageOverRequest => e
-    response = { statusCode: 400, body: e.message }
-    render json: JSON.pretty_generate(response)
   end
 
   # GET /products/1
   def show
+    raise Exceptions::ProductNotFound if @product.items.blank?
+    str_int = @product.items[0]['id']
+    @product.items[0]['id'] = str_int.to_i
     response = { statusCode: 200, body: @product.items }
     render json: JSON.pretty_generate(response)
   end
@@ -80,10 +88,10 @@ class ProductsController < ApplicationController
       raise Exceptions::ParameterIsNotInteger unless is_number? params[:id]
 
       id = params[:id].to_i
-      # raise Exceptions::BadRequest if id <= 0
 
       param = {
         table_name: 'idus-api-prod-products',
+        projection_expression: "id, title, seller, thumbnail_720, thumbnail_list_320, cost, discount_cost, discount_rate, description",
         key_condition_expression: "stat = :stat and id = :id",
         expression_attribute_values: {
           ":stat" => "ok",
--- app/exceptions/exceptions.rb
+++ app/exceptions/exceptions.rb
@@ -4,47 +4,56 @@ module Exceptions
   
     class BadRequest < StandardError
       def status
-        :bad_request
+        400
       end
     end
-  
+
+    class NotFound < StandardError
+      def status
+        404
+      end
+    end
+
     class Unauthorized < StandardError
       def status
-        :unauthorized
+        401
       end
     end
   
     class Forbidden < StandardError
       def status
-        :forbidden
+        403
       end
     end
   
     class InternalServerError < StandardError
       def status
-        :internal_server_error
+        500
       end
-    end
-  
-    class PageOverRequest < BadRequest
-        def message
-            "Request Error - Page is over request !"
-        end
-    end
-
-    class PageUnderRequest < BadRequest
-        def message
-            "Request Error - Page is starting from 1 !"
-        end
     end
 
     class ParameterIsNotInteger < BadRequest
-        def message
-            "Request Error - Parameter is not Integer !"
-        end
+      def message
+        "ParameterIsNotInteger - 요청 변수가 Integer 타입이 아닙니다!"
+      end
+  end
+  
+    class PageOverRequest < NotFound
+      def message
+        "PageOverRequest - 요청 페이지를 초과 했습니다!"
+      end
     end
 
-    # class ProductNotFound < BadRequest
-    # end
+    class PageUnderRequest < NotFound
+      def message
+        "PageUnderRequest - 페이지는 1부터 시작합니다!"
+      end
+    end
+
+    class ProductNotFound < NotFound
+      def message
+        "ProductNotFound - 데이터가 비어있습니다!"
+      end
+    end
   end
   
